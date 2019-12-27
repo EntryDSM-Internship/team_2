5a3a0fb2409478efa3a04da2f0f17fda47c6fc70
JeongWooYeong <wjd030811@gmail.com>
2019-12-27 17:14:54 +0900

    [FIX] fixed profile image routing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,8 +17,7 @@ class UsersController < ApplicationController
                    email: user.email,
                    following: Follow.where(following_id: params[:userId]).count - 1,
                    follower: Follow.where(follower_id: params[:userId]).count - 1,
-                   profile_img: user_img,
-                   tweets: user.tweets.ids[0..9] },
+                   tweets: user.tweets.ids[0..9] }.merge(user_img),
            status: 200
   end
 
