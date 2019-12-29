7e2abecbf340a170af1ea2e8844d976957190a6e
JeongWooYeong <wjd030811@gmail.com>
2019-12-29 23:50:02 +0900

    [FIX] returned previous ver

--- app/models/tweet_img.rb
+++ app/models/tweet_img.rb
@@ -2,5 +2,4 @@ class TweetImg < ApplicationRecord
   belongs_to :tweet
 
   mount_uploader :source, ImageUploader
-  validates file_size: { maximum: 50.megabytes.to_i }
 end
--- app/models/user_img.rb
+++ app/models/user_img.rb
@@ -2,5 +2,4 @@ class UserImg < ApplicationRecord
   belongs_to :user
 
   mount_uploader :source, ImageUploader
-  validates file_size: { maximum: 50.megabytes.to_i }
 end
