0c5a11e93cdefd73428d462e9d18f8d5e96456a9
JeongWooYeong <wjd030811@gmail.com>
2019-12-29 23:39:44 +0900

    [FIX] fixed image size

--- app/models/tweet_img.rb
+++ app/models/tweet_img.rb
@@ -2,4 +2,5 @@ class TweetImg < ApplicationRecord
   belongs_to :tweet
 
   mount_uploader :source, ImageUploader
+  validates file_size: { maximum: 50.megabytes.to_i }
 end
--- app/models/user_img.rb
+++ app/models/user_img.rb
@@ -2,4 +2,5 @@ class UserImg < ApplicationRecord
   belongs_to :user
 
   mount_uploader :source, ImageUploader
+  validates file_size: { maximum: 50.megabytes.to_i }
 end
